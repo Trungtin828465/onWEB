87e0c237870a5948b02d3ba8fb0f6719cafe38e3
TrungTin <Trungtin0972@gmail.com>
2024-12-24 17:31:53 +0700

    a

--- THI_WEB/Controllers/HomeController.cs
+++ THI_WEB/Controllers/HomeController.cs
@@ -71,51 +71,35 @@ namespace THI_WEB.Controllers
             return View(sp);
         }
         [HttpPost]
-        public ActionResult Edit(Sanpham id, HttpPostedFileBase IMG)
+      
+        public ActionResult Edit(Sanpham sp, HttpPostedFileBase Img)
         {
+            QuanLyBanQuanAoEntities db = new QuanLyBanQuanAoEntities();
+            if (Img != null && Img.ContentLength > 0)
+            {
+                string fileName = Path.GetFileName(Img.FileName);
+                string fileExtension = Path.GetExtension(fileName).ToLower();
+
+                if (fileExtension == ".jpg" || fileExtension == ".png")
+                {
+                    string filePath = Path.Combine(Server.MapPath("~/Content/images/"), fileName);
+                    Img.SaveAs(filePath);
+                    sp.AnhDaiDien = fileName;
+                }
+                else
+                {
+                    ModelState.AddModelError("AnhDaiDien", "Chỉ chấp nhận file .jpg hoặc .png.");
+                }
+            }
             if (ModelState.IsValid)
             {
-                var sp = db.Sanphams.Find(id.SanphamID);
-                if (sp == null)
-                {
-                    return HttpNotFound();
-                }
-
-              
-                sp.TenSanpham = id.TenSanpham;
-                sp.MoTa = id.MoTa;
-                sp.Gia = id.Gia;
-                sp.TrangThai = id.TrangThai;
-                sp.NoiBat = id.NoiBat;
-                sp.PhanLoaiSanPhamID = id.PhanLoaiSanPhamID;
-
-                if (IMG != null && IMG.ContentLength > 0)
-                {
-                    string fileName = Path.GetFileName(IMG.FileName);
-                    string fileExtension = Path.GetExtension(fileName).ToLower();
-
-                    if (fileExtension == ".jpg" || fileExtension == ".png")
-                    {
-                        string filePath = Path.Combine(Server.MapPath("~/Content/images/"), fileName);
-                        IMG.SaveAs(filePath);
-                        sp.AnhDaiDien = fileName;
-                    }
-                    else
-                    {
-                        ModelState.AddModelError("IMG", "Chỉ hỗ trợ các định dạng hình ảnh .jpg và .png.");
-                    }
-                }
-
                 db.Sanphams.AddOrUpdate(sp);
                 db.SaveChanges();
-
                 return RedirectToAction("Index");
             }
-
-            ViewBag.category = new SelectList(db.PhanLoaiSanPhams.ToList(), "PhanLoaiSanPhamID", "TenPhanLoai");
-            return View(id);
+            ViewBag.category = new SelectList(db.PhanLoaiSanPhams.ToList(), "PhanLoaiSanPhamID", "TenPhanLoai", sp.PhanLoaiSanPhamID);
+            return View(sp);
         }
-
 
 
         public ActionResult Create()
@@ -128,70 +112,27 @@ namespace THI_WEB.Controllers
         [HttpPost]
         public ActionResult Create(Sanpham sp, HttpPostedFileBase Img)
         {
-           
             if (ModelState.IsValid)
             {
-                // Kiểm tra các trường bắt buộc
-                if (string.IsNullOrEmpty(sp.TenSanpham)==true)
-                {
-                    ModelState.AddModelError("TenSanpham", "Họ tên không được để trống.");
-                }
-               
-
-                else if ( !Regex.IsMatch(sp.TenSanpham, @"^[a-zA-Z]"))
-                {
-                    ModelState.AddModelError("TenSanpham", "Họ tên phải bắt đầu bằng một chữ cái.");
-                }
-
-
-                if (string.IsNullOrEmpty(sp.MoTa))
-                {
-                    ModelState.AddModelError("MoTa", "MoTa không được để trống.");
-                }
-
-                // Kiểm tra giá trị của Giá
-                if (!decimal.TryParse(sp.Gia.ToString(), out decimal giaValue))
-                {
-                    ModelState.AddModelError("Gia", "Giá phải là một số hợp lệ.");
-                }
-                else if (sp.Gia < 0)
-                {
-                    ModelState.AddModelError("Gia", "Giá phải lớn hơn hoặc bằng 0.");
-                }
-
-
-                
-               
                 if (Img != null && Img.ContentLength > 0)
                 {
                     string fileName = Path.GetFileName(Img.FileName);
                     string fileExtension = Path.GetExtension(fileName).ToLower();
-                 
                     if (fileExtension == ".jpg" || fileExtension == ".png")
                     {
                         string filePath = Path.Combine(Server.MapPath("~/Content/images/"), fileName);
                         Img.SaveAs(filePath);
                         sp.AnhDaiDien = fileName;
                     }
-                    else
-                    {
-                        ModelState.AddModelError("Img", "Chỉ hỗ trợ các định dạng hình ảnh .jpg và .png.");
-                    }
                 }
-                else
+               
+                if (ModelState.IsValid)
                 {
-                    ModelState.AddModelError("Img", "Vui lòng chọn một tệp hình ảnh.");
+                    db.Sanphams.Add(sp);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
                 }
-                if (!ModelState.IsValid)
-                {
-                    // Đổ lại ViewBag.category khi có lỗi
-                    ViewBag.category = new SelectList(db.PhanLoaiSanPhams.ToList(), "PhanLoaiSanPhamID", "TenPhanLoai");
-                    return View(sp);
-                }
-                db.Sanphams.Add(sp);
-                db.SaveChanges();
-             
-                return RedirectToAction("Index");
 
             }
             ViewBag.category = new SelectList(db.PhanLoaiSanPhams.ToList(), "PhanLoaiSanPhamID", "TenPhanLoai");
